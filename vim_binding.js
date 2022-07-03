2bb9ea4826e190e2595c60427146d342edb39ce9
JFengl <42263326+JFengl@users.noreply.github.com>
2022-07-03 15:04:53 +0800

    Update vim_binding.js

--- vim_binding.js
+++ vim_binding.js
@@ -69,7 +69,7 @@ define([
     cm_config.keyMap = 'vim';
     cm_config.extraKeys = $.extend(cm_config.extraKeys || {}, {
       'Esc': CodeMirror.prototype.leaveInsertMode,
-      'Command-Esc': CodeMirror.prototype.leaveNormalMode,
+      'Esc-Esc': CodeMirror.prototype.leaveNormalMode,
       'Ctrl-C': false,  // To enable clipboard copy
     });
 
